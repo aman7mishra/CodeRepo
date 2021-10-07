0734c2578407b2055ea5d1d6f4cb5bd7124c6178
Aman <aman@switchon.io>
2021-10-08 00:38:22 +0530

    Two sum

---
+++ leetcode/arrays/two_sum.py
@@ -0,0 +1,26 @@
+"""
+Given an array of intergers, find the index of the two elements that add up to the number provided
+
+Input - [1,2,6,9,7], target - 8
+"""
+
+
+def two_sum(arr, target):
+    """
+    Generates a hashtable to solve the problem.
+    """
+    result = None
+    hash_set = dict()
+    for index, number in enumerate(arr):
+        difference = target - number
+        try:
+            result = (hash_set[difference], index)
+            break
+        except KeyError:
+            hash_set[number] = index
+    return result
+
+print(two_sum([1, 2, 6, 9, 7], 8))
+print(two_sum([2, 7, 11, 15], 9))
+print(two_sum([3, 2, 4], 6))
+print(two_sum([3, 3], 6))
